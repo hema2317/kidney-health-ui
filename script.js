fc5f9abeb52ff0429a473d7790c3530d0cc93211
hema2317 <hemamadhav3@gmail.com>
2025-04-02 13:59:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿document.getElementById("predictForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+
+  const form = new FormData(e.target);
+  const data = {};
+  form.forEach((value, key) => (data[key] = value));
+
+
+  const response = await fetch("https://kidney-health-api.onrender.com/predict", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+
+  const result = await response.json();
+  document.getElementById("result").innerText = `Risk Level: ${result.risk}`;
+});
